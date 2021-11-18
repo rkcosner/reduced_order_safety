b6f3c9169bbca43285a0bf667725c2aa7fbfdea7
rkcosner <rkcosner@caltech.edu>
2021-11-17 16:54:33 -0800

    creating general marker creator function in general pyutils

--- src/utils/general.py
+++ src/utils/general.py
@@ -2,3 +2,48 @@ from geometry_msgs.msg import PoseStamped
 from visualization_msgs.msg import Marker
 
 realsense_offset = 0.26 
+
+
+
+# Create Visualization Markers
+def createMarker(self, id, pose, exists): 
+    marker = Marker()
+
+    # The received positions are in the world frame, 
+    # so we have to shift them forward by the realsense 
+    # offset to get them in the realsense reference frame
+    marker.header.frame_id = "t265_odom_frame"
+    marker.pose.position.x = pose.pose.position.x + realsense_offset
+    marker.pose.position.y = pose.pose.position.y -1
+    marker.pose.position.z = 0.5
+    
+    # Unitree 
+    if id == 0: 
+        marker.type = marker.ARROW
+        marker.scale.x = 0.25
+        marker.scale.z = 0.1
+        marker.scale.y = 0.1
+        marker.color.r = 0.5
+        marker.color.g = 0
+    # Obstacle   
+    else: 
+        marker.type = marker.CYLINDER
+        marker.scale.x = 1
+        marker.scale.y = 1
+        marker.scale.z = 1
+        marker.color.r = 1 
+        marker.color.g = 0.25
+
+    marker.color.b = 0.25
+    marker.color.a = 0.9
+    marker.lifetime = rospy.Duration(0, 10000)
+    marker.pose.orientation.w = pose.pose.orientation.w
+    marker.pose.orientation.z = pose.pose.orientation.z
+    marker.id = id
+    
+    if not exists: 
+        marker.action = marker.ADD
+    else: 
+        marker.action = marker.MODIFY
+
+    return marker
--- src/utils/optitrack_utils.py
+++ src/utils/optitrack_utils.py
@@ -8,9 +8,12 @@ from general import *
 
 class optitrack_vis_node(): 
     def __init__(self):
+
+        # Initialize Node 
         rospy.init_node('optitrack_vis_node', anonymous=True)
         
-        self.rate = rospy.Rate(5) # 10hz
+        # Set up Publishers and subscribers
+        self.rate = rospy.Rate(10) 
         rospy.Subscriber("/vrpn_client_node/Unitree/pose", PoseStamped, self.unitreeCallback)
         rospy.Subscriber("/vrpn_client_node/Tower1/pose", PoseStamped, self.tower1Callback)
         rospy.Subscriber("/vrpn_client_node/Tower2/pose", PoseStamped, self.tower2Callback)
@@ -21,53 +24,33 @@ class optitrack_vis_node():
         self.pubTower2 = rospy.Publisher('/tower2_vis_marker',Marker, queue_size=1)
         self.pubTower3 = rospy.Publisher('/tower3_vis_marker',Marker, queue_size=1)
 
+        # Modify or Create Marker
+        self.unitreeExists = False
+        self.tower1Exists = False
+        self.tower2Exists = False
+        self.tower3Exists = False
 
+    # Callbacks to update visualization markers
     def unitreeCallback(self, msg): 
-        marker = self.createMarker(0,msg)
+        marker = createMarker(0,msg,self.unitreeExists)
         self.pubUnitree.publish(marker)
+        if not self.unitreeExists:
+            self.unitreeExists = True
 
     def tower1Callback(self, msg): 
-        marker = self.createMarker(1,msg) 
+        marker = createMarker(1,msg, self.tower1Exists) 
         self.pubTower1.publish(marker)
+        if not self.tower1Exists:
+            self.tower1Exists = True
 
     def tower2Callback(self, msg): 
-        marker = self.createMarker(2,msg)
+        marker = createMarker(2,msg, self.tower2Exists)
         self.pubTower2.publish(marker)
+        if not self.tower2Exists:
+            self.towe2Exists = True
 
     def tower3Callback(self, msg): 
-        marker = self.createMarker(3,msg)
+        marker = createMarker(3,msg, self.tower3Exists)
         self.pubTower3.publish(marker)
-
-
-
-    def createMarker(self, id, pose): 
-        # Add point at location pt as a marker in the marker_array
-        marker = Marker()
-        marker.header.frame_id = "t265_odom_frame"
-        marker.type = marker.CYLINDER
-        if id == 0: 
-            marker.type = marker.ARROW
-            
-        marker.action = marker.ADD
-        marker.scale.x = 1
-        marker.scale.y = 1
-        marker.scale.z = 1
-        if id == 0: 
-            marker.scale.x = 0.25
-            marker.scale.z = 0.1
-            marker.scale.y = 0.1
-        marker.color.a = 0.9
-        marker.color.r = 1 
-        marker.color.g = 0.25
-        if id == 0: 
-            marker.color.r = 0.5
-            marker.color.g = 0
-        marker.color.b = 0.25
-        marker.lifetime = rospy.Duration(0, 10000)
-        marker.pose.orientation.w = pose.pose.orientation.w
-        marker.pose.orientation.z = pose.pose.orientation.z
-        marker.id = id
-        marker.pose.position.x = pose.pose.position.x + realsense_offset
-        marker.pose.position.y = pose.pose.position.y -1
-        marker.pose.position.z = 0.5
-        return marker
+        if not self.tower3Exists:
+            self.tower3Exists = True
